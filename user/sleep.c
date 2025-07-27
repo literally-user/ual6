a555c54f038f7b867afc39039540aac62d8e7dc1
ltu <literally_user@hotmail.com>
2025-07-27 14:50:22 +0300

    Fix (no zero numbers) problem & added prototypes

--- user/sleep.c
+++ user/sleep.c
@@ -2,8 +2,19 @@
 #include <kernel/stat.h>
 #include <user/user.h>
 
-// TODO: Fix number with no zeros
-// TODO: Write prototypes
+int i_pow(int, int);
+int i_atoi(char*);
+
+int main(int argc, char* argv[]) {
+	int pid = fork();
+
+	if (pid == 0) {
+		sleep(i_atoi(argv[1]) * 10);
+		exit(0);
+	}
+
+	exit(0);
+}
 
 int i_pow(int a, int b) {
 	if (b == 0) {
@@ -20,23 +31,9 @@ int i_atoi(char* a) {
 		;
 	}
 
-	for (int i=0; len != 1; ++i) {
-		char num = a[i] - 48;
-		result += (i_pow(10, len--) * num);
+	for (int i=0; len != 0; ++i) {
+		result += (i_pow(10, len--) * (a[i] - 48));
 	}
 
 	return result;
-}
-
-int main(int argc, char* argv[]) {
-	int pid = fork();
-
-	if (pid == 0) {
-		sleep(i_atoi(argv[1]) * 10);
-		exit(0);
-	} else {
-		wait(0);
-	}
-
-	exit(0);
 }
