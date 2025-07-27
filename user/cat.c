aba90f820df56687a594f7b87b8646016e11e927
ltu <literally_user@hotmail.com>
2025-07-27 14:47:01 +0300

    Some changes

--- user/cat.c
+++ user/cat.c
@@ -4,8 +4,12 @@
 
 char buf[512];
 
-void cat(int fd) {
-  while((int n = read(fd, buf, sizeof(buf))) > 0) {
+void
+cat(int fd)
+{
+  int n;
+
+  while((n = read(fd, buf, sizeof(buf))) > 0) {
     if (write(1, buf, n) != n) {
       fprintf(2, "cat: write error\n");
       exit(1);
@@ -17,16 +21,17 @@ void cat(int fd) {
   }
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-  int fd;
+  int fd, i;
 
   if(argc <= 1){
     cat(0);
     exit(0);
   }
 
-  for(int i = 1; i < argc; i++){
+  for(i = 1; i < argc; i++){
     if((fd = open(argv[i], O_RDONLY)) < 0){
       fprintf(2, "cat: cannot open %s\n", argv[i]);
       exit(1);
@@ -34,6 +39,5 @@ int main(int argc, char *argv[])
     cat(fd);
     close(fd);
   }
-
   exit(0);
 }
--- user/forktest.c
+++ user/forktest.c
@@ -7,34 +7,40 @@
 
 #define N  1000
 
-void print(const char *s) {
+void
+print(const char *s)
+{
   write(1, s, strlen(s));
 }
 
-void forktest(void) {
+void
+forktest(void)
+{
+  int n, pid;
+
   print("fork test\n");
 
-  for(int n=0; n<N; n++) {
-    int pid = fork();
+  for(n=0; n<N; n++){
+    pid = fork();
     if(pid < 0)
       break;
     if(pid == 0)
       exit(0);
   }
 
-  if(n == N) {
+  if(n == N){
     print("fork claimed to work N times!\n");
     exit(1);
   }
 
-  for(; n > 0; n--) {
+  for(; n > 0; n--){
     if(wait(0) < 0){
       print("wait stopped early\n");
       exit(1);
     }
   }
 
-  if(wait(0) != -1) {
+  if(wait(0) != -1){
     print("wait got too many\n");
     exit(1);
   }
@@ -42,7 +48,9 @@ void forktest(void) {
   print("fork test OK\n");
 }
 
-int main(void) {
+int
+main(void)
+{
   forktest();
   exit(0);
 }
