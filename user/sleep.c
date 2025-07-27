bc78675cd2abe18251c3d5a9ef37d5d4d484a37e
Literally User <literally_user@hotmail.com>
2025-07-27 17:06:03 +0300

    Remove custom atoi

--- user/sleep.c
+++ user/sleep.c
@@ -3,13 +3,12 @@
 #include <user/user.h>
 
 int i_pow(int, int);
-int i_atoi(char*);
 
 int main(int argc, char* argv[]) {
 	int pid = fork();
 
 	if (pid == 0) {
-		sleep(i_atoi(argv[1]) * 10);
+		sleep(atoi(argv[1]) * 10);
 		exit(0);
 	}
 
@@ -22,18 +21,4 @@ int i_pow(int a, int b) {
 	}
 
 	return a * i_pow(a, b - 1);
-}
-
-int i_atoi(char* a) {
-	int len, result = 0;
-
-	for (len=0; a[len]!='\0'; ++len) {
-		;
-	}
-
-	for (int i=0; len != 0; ++i) {
-		result += (i_pow(10, len--) * (a[i] - 48));
-	}
-
-	return result;
 }
