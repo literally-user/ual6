a49a334180cd6613cdd120e56ad6868edcf8c0ce
Literally User <literally_user@hotmail.com>
2025-07-28 01:21:26 +0300

    Update fs.c

--- kernel/fs.c
+++ kernel/fs.c
@@ -1,10 +1,15 @@
 // File system implementation.  Five layers:
 //   + Blocks: allocator for raw disk blocks.
+
 //   + Log: crash recovery for multi-step updates.
+
 //   + Files: inode allocator, reading, writing, metadata.
+
 //   + Directories: inode with special contents (list of other inodes!)
-//   + Names: paths like /usr/rtm/xv6/fs.c for convenient naming.
+
+//   + Names: paths like /usr/rtm/ual6/fs.c for convenient naming.
 //
+
 // This file contains the low-level file system manipulation
 // routines.  The (higher-level) system call implementations
 // are in sysfile.c.
