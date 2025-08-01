78c3dcb3b0b99b422aa995500a62127b16228e03
ltu <literally_user@hotmail.com>
2025-08-01 21:15:35 +0300

    Write prototypes

--- user/cat.c
+++ user/cat.c
@@ -4,8 +4,29 @@
 
 char buf[512];
 
-void
-cat(int fd)
+void cat(int);
+
+int main(int argc, char *argv[]) {
+	int fd, i;
+
+	if (argc <= 1) {
+		cat(0);
+		exit(0);
+	}
+	
+	for (i=0; i < argc; i++) {
+		if ((fd = open(argv[1], O_RDONLY)) < 0) {
+			fprintf(2, "cat: cannot open %s\n", argv[i]);
+			exit(1);
+		}
+		cat(fd);
+		close(fd);
+	}
+
+	exit(0);
+}
+
+void cat(int fd)
 {
   int n;
 
@@ -21,23 +42,22 @@ cat(int fd)
   }
 }
 
-int
-main(int argc, char *argv[])
-{
-  int fd, i;
-
-  if(argc <= 1){
-    cat(0);
-    exit(0);
-  }
-
-  for(i = 1; i < argc; i++){
-    if((fd = open(argv[i], O_RDONLY)) < 0){
-      fprintf(2, "cat: cannot open %s\n", argv[i]);
-      exit(1);
-    }
-    cat(fd);
-    close(fd);
-  }
-  exit(0);
-}
+//int main(int argc, char *argv[])
+//{
+//  int fd, i;
+//
+//  if(argc <= 1){
+//    cat(0);
+//    exit(0);
+//  }
+//
+//  for(i = 1; i < argc; i++){
+//    if((fd = open(argv[i], O_RDONLY)) < 0){
+//      fprintf(2, "cat: cannot open %s\n", argv[i]);
+//      exit(1);
+//    }
+//    cat(fd);
+//    close(fd);
+//  }
+//  exit(0);
+//}
